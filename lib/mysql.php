1945e19f8bb73c7754ca7dc6595f2106195e957b
d7d7zd <1020837120@qq.com>
2017-04-09 16:05:40 +0800

    base 0.0.1

---
+++ lib/mysql.php
@@ -0,0 +1,89 @@
+<?php
+require_once "../config/mysql.config.php";
+
+//cnnect mysql
+function connect(){
+	$link = mysqli_connect(Localhost, User, Password, Db);
+	mysqli_set_charset($link, "utf8");
+	return $link;
+}
+
+//insert into data back id
+function insert($table, $array){
+	$link = connect();
+	$keys =join(",", array_keys($array));
+	$values = "'".join("','", array_values($array))."'";
+	//echo $values;
+	$sql = "insert into {$table} ({$keys}) values($values)";
+	$num = mysqli_query($link, $sql);
+	mysqli_close($link);
+	return $num;
+}
+
+
+//delete data
+function delete($table, $where = null){
+	$where = $where==null? null :" where ".$where;
+	$link = connect();	
+	$sql = "delete from {$table} {$where}";
+	//echo $sql;
+	$num = mysqli_query($link, $sql);
+	mysqli_close($link);
+	return $num;
+}   
+
+//select data
+function select($table, $where=null, $isAll=false){
+	$where =$where==null ? null : " where ".$where;
+	$link = connect();
+	$sql = "select *from {$table} {$where}";
+	$res = mysqli_query($link, $sql);
+	if($isAll == true){ 
+		while($row =mysqli_fetch_assoc($res)){
+			$rows[] = $row;
+		}
+		mysqli_close($link);
+		return $rows;				
+	}else {
+		$row= mysqli_fetch_assoc($res);		
+		mysqli_close($link);
+		return $row;
+	}
+}
+
+//select data all
+function selectAll($table, $where=null){
+	return select($table, $where, true);
+}
+
+//update data
+function update($table, $array, $where=null){
+	$where = $where ==null ? null : " where ".$where;
+	$link = connect();
+	$str = '';
+	foreach($array as $key=>$value){
+		if($str == ''){
+			$step = '';
+		} else {
+			$step = ',';
+		}
+		$str = $step.$key."='".$value."'";
+	}
+	$sql = "update {$table} set {$str}".$where;
+	echo $sql;
+	$num = mysqli_query($link, $sql);
+	mysqli_close($link);
+	return $num; 
+}
+
+//测试代码
+//var_dump(update("admin", array('password'=>'555'),"dname='root'"));
+//var_dump(select("admin"));
+//var_dump(selectAll("admin"));
+//var_dump(delete("admin", "dname='root'"));
+//var_dump($link = connect());
+//$link = connect();
+//if($link){
+//	echo "get it";
+//}
+//echo insert("admin", array('dname'=>'zhang', 'password'=>'6063078'));
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
-1.0.0 master
+0.0.1 master
 nothing
-
+	base
+		0.0.1
+		生成数据库表，完成数据操作的机本操作。
